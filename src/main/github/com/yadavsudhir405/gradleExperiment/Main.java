176b71f48ed3f8ed9f496bffe8fb97e5745ae5a0
Sudhir <yadavsudhir405@gmail.com>
2017-08-22 23:33:52 +0530

    Add Main java files

--- src/main/github/com/yadavsudhir405/gradleExperiment/Main.java
+++ src/main/github/com/yadavsudhir405/gradleExperiment/Main.java
@@ -1,9 +1,12 @@
 package github.com.yadavsudhir405.gradleExperiment;
 import java.util.*;
-class Main{
+public class Main{
 	
 	public static void main(String[] args){
-		System.out.println("Hello world")
-		System.out.println(Arrays.asList(args))
+		System.out.println("Hello world");
+		for(String str : args){
+			System.out.println(str);
+		}
+		
 	}
 }
