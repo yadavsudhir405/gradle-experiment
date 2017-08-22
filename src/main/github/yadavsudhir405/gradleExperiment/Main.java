7c31a3ae0f0768c5a54750bee1975affa5ed01fc
Sudhir <yadavsudhir405@gmail.com>
2017-08-22 22:30:21 +0530

    First commit

---
+++ src/main/github/yadavsudhir405/gradleExperiment/Main.java
@@ -0,0 +1,9 @@
+package github.com.yadavsudhir405.gradleExperiment;
+import java.util.*;
+class Main{
+	
+	public static void main(String[] args){
+		System.out.println("Hello world")
+		System.out.println(Arrays.asList(args))
+	}
+}
